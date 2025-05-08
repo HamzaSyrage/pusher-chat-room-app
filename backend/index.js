b1ecbfddf7035f1c9aa6d2cc020f035f967fa99c
HamzaSyrage <hamzasyrage@gmail.com>
2025-05-08 10:15:11 +0300

    deprecated Use authorizeChannel

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.post("/pusher/auth", (req, res) => {
 	};
 
 	// Authorize the subscription
-	const auth = pusher.authenticate(socket_id, channel_name, presenceData);
+	const auth = pusher.authorizeChannel(socket_id, channel_name, presenceData);
 	res.send(auth);
 });
 
